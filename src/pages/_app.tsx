cda995b51fc57237beda43dfe920ee6bb540e83e
mkataw8 <mkataw8@gmail.com>
2023-06-07 20:57:35 -0400

    Fix: Firebase Error

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,29 @@
+import { initializeApp, type FirebaseApp } from "firebase/app";
+import "firebase/auth";
+import "firebase/firestore";
 import { type AppType } from "next/dist/shared/lib/utils";
-// import { initFirebase } from "~/Firebase/firebaseapp";
-import { initializeApp } from "firebase/app";
+
 import "~/styles/globals.css";
 
+// Initialize Firebase with your configuration
+const firebaseConfig = {
+  // Add your Firebase configuration options here
+  apiKey: "YOUR_API_KEY",
+  authDomain: "YOUR_AUTH_DOMAIN",
+  projectId: "YOUR_PROJECT_ID",
+  // ...
+};
+
+let firebaseApp: FirebaseApp;
+
+const initializeFirebase = () => {
+  if (!firebaseApp) {
+    firebaseApp = initializeApp(firebaseConfig);
+  }
+};
+
 const MyApp: AppType = ({ Component, pageProps }) => {
-  initializeApp();
+  initializeFirebase();
 
   return <Component {...pageProps} />;
 };
