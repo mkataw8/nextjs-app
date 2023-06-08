9ae7da99aa1f3d0d8c0ecc15116d6e87001ff17b
mkataw8 <mkataw8@gmail.com>
2023-06-07 20:50:50 -0400

    Error: Fix

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,10 @@
 import { type AppType } from "next/dist/shared/lib/utils";
-import { initFirebase } from "~/Firebase/firebaseapp";
-
+// import { initFirebase } from "~/Firebase/firebaseapp";
+import { initializeApp } from "firebase/app";
 import "~/styles/globals.css";
 
 const MyApp: AppType = ({ Component, pageProps }) => {
-  initFirebase();
+  initializeApp();
 
   return <Component {...pageProps} />;
 };
